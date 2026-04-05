4b77e350ceac12b6fe1a67e9f2c500ffa88a6330
ADIL102214 <2024831066@student.sust.edu>
2026-04-05 15:17:29 +0600

    Task no. 1

---
+++ Task_101.c
@@ -0,0 +1,104 @@
+#include <SDL2/SDL.h>
+#include<stdio.h>
+
+#define FALSE 0
+#define TRUE 1
+#define WINDOW_WIDTH 700
+#define WINDOW_HEIGHT 600
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+int game_is_running = FALSE;
+
+int initialize_window(){
+    if(SDL_Init(SDL_INIT_EVERYTHING)!=0){
+        fprintf(stderr, "ERROR INITIALIZING SDL.\n");
+        return FALSE;
+    }
+    window = SDL_CreateWindow(
+        "TASK_101",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        0
+    );
+    if(!window){
+        fprintf(stderr, "ERROR INITIALIZING SDL Window\n");
+        return FALSE;
+    }
+    renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED
+    );
+    if(!renderer){
+        fprintf(stderr, "Error creating SDL renderer\n");
+        return FALSE;
+    }
+    return TRUE;
+}
+
+void process_input(){
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch(event.type){
+        case SDL_QUIT:
+        game_is_running = FALSE;
+        break;
+        case SDL_KEYDOWN:
+        if (event.key.keysym.sym == SDLK_ESCAPE) game_is_running = FALSE;
+        break;
+         
+    }
+}
+
+
+void drawCircle(SDL_Renderer *renderer, int centerX, int centerY, int radius){
+    int x =radius;
+    int y = 0;
+    int error = 1-radius;
+    while(x>=y){
+        SDL_RenderDrawPoint(renderer, centerX+x, centerY-y);
+        SDL_RenderDrawPoint(renderer, centerX+x, centerY+y);
+        SDL_RenderDrawPoint(renderer, centerX-x, centerY-y);
+        SDL_RenderDrawPoint(renderer, centerX-x, centerY+y);
+        SDL_RenderDrawPoint(renderer, centerX+y, centerY-x);
+        SDL_RenderDrawPoint(renderer, centerX+y, centerY+x);
+        SDL_RenderDrawPoint(renderer, centerX-y, centerY-x);
+        SDL_RenderDrawPoint(renderer, centerX-y, centerY+x);
+        if(error<=0){
+            y++;
+            error+=2*y+1;
+        }if(error>0){
+            x--;
+            y++;
+            error+=2*y-2*x+1;
+        }
+    } 
+}
+
+void draw(){
+    SDL_SetRenderDrawColor(renderer,50,50,0,255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer,255,255,255,255);
+    drawCircle(renderer, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 240);
+    SDL_RenderPresent(renderer);
+}
+
+void destroy_window(){
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]){
+    game_is_running = initialize_window();
+    while(game_is_running){
+        process_input();
+        draw();
+        SDL_Delay(16);
+    }
+    destroy_window();
+    return 0;
+}
